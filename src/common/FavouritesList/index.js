e93943979c32af265413a2d47f157fadd0670569
Gosia_Ras <ras.malgorzata@gmail.com>
2022-03-14 13:43:13 +0100

    swiper removed

--- src/common/FavouritesList/index.js
+++ src/common/FavouritesList/index.js
@@ -5,59 +5,26 @@ import {
   MoviesContainer,
 } from "../MovieList/styled";
 import { nanoid } from "nanoid";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 export const FavouritesList = (props) => {
   const FavouriteComponent = props.favouriteComponent;
   return (
     <MoviesContainer id="watch">
-      {props.movies.length < 4 ? (
-        props.movies.map((movie) => {
-          return (
-            <ImageContainer
-              key={nanoid()}
-              onClick={() => props.handleFavouritesClick(movie)}
-            >
-              <Poster src={movie.Poster} alt="movie" />
-              <p>{movie.Title}</p>
-              <p>{movie.Year}</p>
-              <Overlay>
-                <FavouriteComponent />
-              </Overlay>
-            </ImageContainer>
-          );
-        })
-      ) : (
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={4}
-          modules={[Navigation, Pagination]}
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-        >
-          {props.movies.map((movie) => {
-            return (
-              <SwiperSlide>
-                <ImageContainer
-                  key={nanoid()}
-                  onClick={() => props.handleFavouritesClick(movie)}
-                >
-                  <Poster src={movie.Poster} alt="movie" />
-                  <p>{movie.Title}</p>
-                  <p>{movie.Year}</p>
-                  <Overlay>
-                    <FavouriteComponent />
-                  </Overlay>
-                </ImageContainer>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      )}
+      {props.movies.map((movie) => {
+        return (
+          <ImageContainer
+            key={nanoid()}
+            onClick={() => props.handleFavouritesClick(movie)}
+          >
+            <Poster src={movie.Poster} alt="movie" />
+            <p>{movie.Title}</p>
+            <p>{movie.Year}</p>
+            <Overlay>
+              <FavouriteComponent />
+            </Overlay>
+          </ImageContainer>
+        );
+      })}
     </MoviesContainer>
   );
 };
--- src/common/MovieList/index.js
+++ src/common/MovieList/index.js
@@ -6,46 +6,27 @@ import {
   SearchIcon,
 } from "./styled";
 import { nanoid } from "nanoid";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 export const MovieList = (props) => {
   const FavouriteComponent = props.favouriteComponent;
   return (
     <MoviesContainer id="browser">
-      {props.movies.length > 0 ? (
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={60}
-          modules={[Pagination]}
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          updateOnWindowResize={true}
-        >
-          {props.movies.map((movie) => {
-            return (
-              <SwiperSlide>
-                <ImageContainer
-                  key={nanoid()}
-                  onClick={() => props.handleFavouritesClick(movie)}
-                >
-                  <Poster src={movie.Poster} alt="movie" />
-                  <p>{movie.Title}</p>
-                  <p>{movie.Year}</p>
-                  <Overlay>
-                    <FavouriteComponent />
-                  </Overlay>
-                </ImageContainer>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      ) : (
-        <SearchIcon />
-      )}
+      {props.movies.map((movie) => {
+        return (
+          <ImageContainer
+            key={nanoid()}
+            onClick={() => props.handleFavouritesClick(movie)}
+          >
+            <Poster src={movie.Poster} alt="movie" />
+            <p>{movie.Title}</p>
+            <p>{movie.Year}</p>
+            <Overlay>
+              <FavouriteComponent />
+            </Overlay>
+          </ImageContainer>
+        );
+      })}
+      <SearchIcon />
     </MoviesContainer>
   );
 };
